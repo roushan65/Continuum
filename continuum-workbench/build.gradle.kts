ee6068f7ac7c4a3261e4c4698ccd60ad2a7b38a1
roushanatlcci <roushan@lilly.com>
2025-01-06 11:12:37 +0530

    add Theia module

---
+++ continuum-workbench/build.gradle.kts
@@ -0,0 +1,28 @@
+import com.github.gradle.node.yarn.task.YarnTask
+
+plugins {
+    id("com.github.node-gradle.node") version "3.2.1"
+}
+
+node {
+    version.set("22.12.0") // Specify the Node.js version
+    yarnVersion.set("1.22.22") // Specify the Yarn version
+    download.set(true) // Automatically download and install Node.js
+}
+
+tasks.register<YarnTask>("yarnInstall") {
+    args.set(listOf("install"))
+}
+
+tasks.register<YarnTask>("build") {
+    dependsOn("yarnInstall")
+    args.set(listOf("run", "build"))
+}
+
+tasks.register<Delete>("clean") {
+    delete("build")
+}
+
+tasks.named("build") {
+    dependsOn("clean")
+}
---
+++ continuum-workbench/src-gen/backend/main.js
@@ -0,0 +1,75 @@
+// @ts-check
+const { BackendApplicationConfigProvider } = require('@theia/core/lib/node/backend-application-config-provider');
+const main = require('@theia/core/lib/node/main');
+
+BackendApplicationConfigProvider.set({
+    "singleInstance": true,
+    "frontendConnectionTimeout": 0,
+    "configurationFolder": ".theia"
+});
+
+globalThis.extensionInfo = [
+    {
+        "name": "@theia/core",
+        "version": "1.57.1"
+    },
+    {
+        "name": "@theia/filesystem",
+        "version": "1.57.1"
+    },
+    {
+        "name": "@theia/variable-resolver",
+        "version": "1.57.1"
+    },
+    {
+        "name": "@theia/workspace",
+        "version": "1.57.1"
+    },
+    {
+        "name": "@theia/markers",
+        "version": "1.57.1"
+    },
+    {
+        "name": "@theia/messages",
+        "version": "1.57.1"
+    },
+    {
+        "name": "@theia/editor",
+        "version": "1.57.1"
+    },
+    {
+        "name": "@theia/outline-view",
+        "version": "1.57.1"
+    },
+    {
+        "name": "@theia/monaco",
+        "version": "1.57.1"
+    },
+    {
+        "name": "@theia/navigator",
+        "version": "1.57.1"
+    },
+    {
+        "name": "@theia/userstorage",
+        "version": "1.57.1"
+    },
+    {
+        "name": "@theia/preferences",
+        "version": "1.57.1"
+    },
+    {
+        "name": "@theia/process",
+        "version": "1.57.1"
+    }
+];
+
+const serverModule = require('./server');
+const serverAddress = main.start(serverModule());
+
+serverAddress.then((addressInfo) => {
+    if (process && process.send && addressInfo) {
+        process.send(addressInfo);
+    }
+});
+
+globalThis.serverAddress = serverAddress;
