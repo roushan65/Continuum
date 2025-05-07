4ce68555109017e74a08c28dacdfb94daa7354f5
roushanatlcci <roushan@lilly.com>
2025-05-07 17:14:51 +0530

    forwarding messages to mosquitto

--- continuum-message-bridge/src/main/kotlin/com/continuum/core/bridge/handler/WorkflowExecutionSnapshotHandler.kt
+++ continuum-message-bridge/src/main/kotlin/com/continuum/core/bridge/handler/WorkflowExecutionSnapshotHandler.kt
@@ -1,17 +1,25 @@
 package com.continuum.core.bridge.handler
 
 import com.continuum.core.commons.model.WorkflowUpdateEvent
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.eclipse.paho.client.mqttv3.MqttClient
+import org.eclipse.paho.client.mqttv3.MqttMessage
 import org.springframework.context.annotation.Bean
+import org.springframework.kafka.support.KafkaHeaders
 import org.springframework.messaging.Message
 import org.springframework.stereotype.Component
 import reactor.core.publisher.Flux
 import java.util.function.Consumer
 
 @Component
-class WorkflowExecutionSnapshotHandler {
+class WorkflowExecutionSnapshotHandler(
+    private val mqttClient: MqttClient
+) {
 
     companion object {
         private val LOGGER = org.slf4j.LoggerFactory.getLogger(WorkflowExecutionSnapshotHandler::class.java)
+        private val MQTT_TOPIC_PREFIX = "continuum/workflow/execution"
+        private val objectMapper = ObjectMapper()
     }
 
     @Bean("continuum-core-event-WorkflowExecutionSnapshot-input")
@@ -26,9 +34,20 @@ class WorkflowExecutionSnapshotHandler {
     }
 
     fun handle(message: Message<WorkflowUpdateEvent>) {
-        // Handle the message here
-        // For example, you can log the message or process it further
-        println("Received message: ${message.payload}")
+        val messagePayloads = objectMapper
+            .writeValueAsString(message.payload)
+            .toByteArray(Charsets.UTF_8)
+        println("Received message: ${String(messagePayloads)}")
+        val workflowId = message.headers[KafkaHeaders.RECEIVED_KEY] as String
+        val mqttMessage = MqttMessage().apply {
+            payload = messagePayloads
+            qos = 1
+            isRetained = true
+        }
+        mqttClient.publish(
+            "$MQTT_TOPIC_PREFIX/$workflowId/update",
+            mqttMessage
+        )
     }
 
 }
--- continuum-worker/src/main/kotlin/com/continuum/core/worker/utils/StatusHelper.kt
+++ continuum-worker/src/main/kotlin/com/continuum/core/worker/utils/StatusHelper.kt
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Profile
 import org.springframework.kafka.support.KafkaHeaders
 import org.springframework.messaging.support.MessageBuilder
 import org.springframework.stereotype.Component
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.springframework.kafka.support.serializer.JsonSerializer
 
 @Profile("kafka_event")
 @Component
@@ -17,37 +19,12 @@ class StatusHelper(
 ) {
     companion object {
         private val LOGGER = LoggerFactory.getLogger(StatusHelper::class.java)
-//        private val objectMapper = ObjectMapper()
-//        private val MQTT_TOPIC_PREFIX = "continuum/workflow/execution"
-//        private val MQTT_CLIENT_ID = System.getenv().getOrDefault("MQTT_CLIENT_ID", UUID.randomUUID().toString())
-//        private val options = MqttConnectOptions().apply {
-//            isAutomaticReconnect = true
-//            isCleanSession = true
-//            connectionTimeout = 10
-//
-//        }
-//        private val mqttClient = MqttClient("tcp://localhost:31883", MQTT_CLIENT_ID).also {
-//            it.connect(options)
-//        }
         private var streamBridge: StreamBridge? = null
 
         fun publishWorkflowSnapshot(
             workflowId: String,
             workflowSnapshot: WorkflowUpdateEvent
         ) {
-//            while(!mqttClient.isConnected) {
-//                LOGGER.warn("MQTT client is not connected. Waiting for updates to continue...")
-//                Thread.sleep(1000)
-//            }
-//            LOGGER.info("Publishing workflow snapshot for workflowId: $workflowId ...")
-//            mqttClient.publish(
-//                "$MQTT_TOPIC_PREFIX/$workflowId/update",
-//                MqttMessage().apply {
-//                    payload = objectMapper.writeValueAsString(workflowSnapshot).toByteArray(Charsets.UTF_8)
-//                    qos = 1
-//                    isRetained = true
-//                }
-//            )
 
             if(streamBridge != null) {
                 try {
